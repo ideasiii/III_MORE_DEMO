5dae539d37408530c1a863e99a42a8044ac1a7dc
Shun-An Lee <andysalee@iii.org.tw>
2017-11-06 16:16:45 +0800

    feed data from CockpitService to InterruptLogicHandler

--- app/src/main/java/com/iii/more/main/MainApplication.java
+++ app/src/main/java/com/iii/more/main/MainApplication.java
@@ -12,11 +12,17 @@ import com.iii.more.cmp.semantic.SemanticDeviceID;
 import com.iii.more.cockpit.CockpitService;
 import com.iii.more.cockpit.InternetCockpitService;
 import com.iii.more.interrupt.logic.InterruptLogicHandler;
+import com.iii.more.interrupt.logic.InterruptLogicParameters;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import sdk.ideas.common.Logs;
+import sdk.ideas.common.OnCallbackResult;
 
 import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 
@@ -35,12 +41,15 @@ public class MainApplication extends Application
     private final List<CockpitConnectionEventListener> mCockpitConnectionEventListeners = new CopyOnWriteArrayList<>();
     private final List<CockpitSensorEventListener> mCockpitSensorEventListeners = new CopyOnWriteArrayList<>();
 
+    private InterruptLogicHandler mInterruptLogicHandler = null;
+
     @Override
     public void onCreate()
     {
         super.onCreate();
 
         bootCockpitService();
+        initInterruptLogic();
     }
 
     /** 取得章魚 or 使用者的名字 */
@@ -91,6 +100,56 @@ public class MainApplication extends Application
                 mCockpitServiceConnection, null);
     }
 
+    private void initInterruptLogic()
+    {
+        mInterruptLogicHandler = new InterruptLogicHandler(this);
+
+        try
+        {
+            SharedPreferences prefs = getDefaultSharedPreferences(getApplicationContext());
+            String message = prefs.getString(Parameters.TASK_COMPOSER_DATA, "ssss");
+            JSONObject tmp = new JSONObject(message);
+
+            if (tmp.has("rules"))
+            {
+                JSONObject rules = tmp.getJSONObject("rules");
+                if (rules.has("action"))
+                {
+                    Logs.showTrace("[MainApplication] Use interrupt logic behavior data array input from SharedPreferences");
+
+                    mInterruptLogicHandler.setInterruptLogicBehaviorDataArray(rules.getJSONArray("action").toString());
+                }
+                else
+                {
+                    Logs.showError("[MainApplication] SharedPreferences does not have interrupt logic behavior data array, use fallback input");
+                    mInterruptLogicHandler.setInterruptLogicBehaviorDataArray(INTERRUPT_LOGIC_BEHAVIOR_DATA_ARRAY_FALLBACK_INPUT);
+                }
+                /*if (rules.has("emotion"))
+                {
+                    mInterruptLogicHandler.setInterruptEmotionLogicBehaviorDataArray(rules.getJSONArray("emotion").toString());
+                }*/
+            }
+        }
+        catch (JSONException e)
+        {
+            Logs.showError("[MainApplication] cannot get interrupt logic behavior data array from SharedPreferences: " + e.toString());
+            Logs.showError("[MainApplication] use fallback input");
+            mInterruptLogicHandler.setInterruptLogicBehaviorDataArray(INTERRUPT_LOGIC_BEHAVIOR_DATA_ARRAY_FALLBACK_INPUT);
+        }
+
+        mInterruptLogicHandler.setHandler(mInterruptLogicHandlerResultHandler);
+        mInterruptLogicHandler.setOnCallbackResultListener(new OnCallbackResult()
+                                                           {
+                                                               @Override
+                                                               public void onCallbackResult(int i, int i1, int i2, HashMap<String, String> hashMap)
+                                                               {
+                                                                   Logs.showTrace("[MainApplication] initInterruptLogic() onCallbackResult()");
+                                                               }
+                                                           }
+        );
+        Logs.showTrace("[MainApplication] initInterruptLogic() OK");
+    }
+
     /** CockpitService 的 ServiceConnection */
     private final ServiceConnection mCockpitServiceConnection = new ServiceConnection()
     {
@@ -136,10 +195,16 @@ public class MainApplication extends Application
                     String data = (String) msg.obj;
                     Logs.showTrace("[MainApplication] mCockpitServiceHandler onData(), data=`" + data + "`");
 
-                    for (CockpitConnectionEventListener l : mCockpitConnectionEventListeners)
+                    if (mInterruptLogicHandler != null)
+                    {
+                        mInterruptLogicHandler.setDeviceEventData(data);
+                        mInterruptLogicHandler.startEventDataAnalysis();
+                    }
+
+                    /*for (CockpitConnectionEventListener l : mCockpitConnectionEventListeners)
                     {
                         l.onData(null, data);
-                    }
+                    }*/
                     break;
                 case CockpitService.MSG_NO_DEVICE:
                     Logs.showTrace("[MainApplication] mCockpitServiceHandler onNoDevice()");
@@ -196,5 +261,58 @@ public class MainApplication extends Application
             }
         }
     };
-    
+
+    private final Handler mInterruptLogicHandlerResultHandler = new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg)
+        {
+            Logs.showTrace("[MainApplication] [mInterruptLogicHandlerResultHandler] handleMessage()");
+            HashMap<String, String> message = (HashMap<String, String>) msg.obj;
+
+            switch (msg.arg2)
+            {
+                case InterruptLogicParameters.METHOD_LOGIC_RESPONSE:
+                    String trigger_result = message.get(InterruptLogicParameters.JSON_STRING_DESCRIPTION);
+
+                    switch (trigger_result)
+                    {
+                        case "握手":
+                            Logs.showTrace("[MainApplication] [mInterruptLogicHandlerResultHandler] shake hands");
+                            for (CockpitSensorEventListener l : mCockpitSensorEventListeners)
+                            {
+                                l.onShakeHands(null);
+                            }
+                            break;
+                        case "拍手":
+                            Logs.showTrace("[MainApplication] [mInterruptLogicHandlerResultHandler] clap hands");
+                            for (CockpitSensorEventListener l : mCockpitSensorEventListeners)
+                            {
+                                l.onClapHands(null);
+                            }
+                            break;
+                        case "擠壓":
+                            Logs.showTrace("[MainApplication] [mInterruptLogicHandlerResultHandler] pinch cheeks");
+                            for (CockpitSensorEventListener l : mCockpitSensorEventListeners)
+                            {
+                                l.onPinchCheeks(null);
+                            }
+                            break;
+                        case "拍頭":
+                            Logs.showTrace("[MainApplication] [mInterruptLogicHandlerResultHandler] pat head");
+                            for (CockpitSensorEventListener l : mCockpitSensorEventListeners)
+                            {
+                                l.onPatHead(null);
+                            }
+                            break;
+                        default:
+                            Logs.showTrace("[MainApplication] [mInterruptLogicHandlerResultHandler] handleMessage() unknown trigger_result: " + trigger_result);
+                    }
+                default:
+                    Logs.showTrace("[MainApplication] [mInterruptLogicHandlerResultHandler] handleMessage() unknown msg.arg2: " + msg.arg2);
+            }
+        }
+    };
+
+    private static final String INTERRUPT_LOGIC_BEHAVIOR_DATA_ARRAY_FALLBACK_INPUT = "[{\"sensors\":[\"f1\",\"f2\",\"C\",\"D\"],\"trigger_rule\":1,\"action\":1,\"tag\":\"SHAKE_HANDS\",\"trigger\":\"OCTOBO_Expressions-35.png\",\"value\":\"1\",\"desc\":\"握手\"},{\"sensors\":[\"f1\",\"f2\",\"C\",\"D\"],\"trigger_rule\":2,\"action\":2,\"tag\":\"CLAP_HANDS\",\"trigger\":\"OCTOBO_Expressions-24.png\",\"value\":\"1\",\"desc\":\"拍手\"},{\"sensors\":[\"FSR1\",\"FSR2\"],\"trigger_rule\":2,\"action\":3,\"tag\":\"EXTRUSION\",\"trigger\":\"OCTOBO_Expressions-01.png\",\"value\":\"1\",\"desc\":\"擠壓\"},{\"sensors\":[\"X\",\"Y\",\"Z\"],\"trigger_rule\":1,\"action\":4,\"tag\":\"SHAKE\",\"trigger\":\"OCTOBO_Expressions-38.png\",\"value\":\"1\",\"desc\":\"搖晃\"},{\"sensors\":[\"H\"],\"trigger_rule\":1,\"action\":5,\"tag\":\"TURN_ON_THE_LIGHT\",\"trigger\":\"ON\",\"value\":\"1\",\"desc\":\"開燈\"},{\"sensors\":[\"FSR1\",\"FSR2\"],\"trigger_rule\":1,\"action\":6,\"tag\":\"PAT_HEAD\",\"trigger\":\"OCTOBO_Expressions-01.png\",\"value\":\"1\",\"desc\":\"拍頭\"}]";
 }
