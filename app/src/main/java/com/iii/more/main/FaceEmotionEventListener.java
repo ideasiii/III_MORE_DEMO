5fb941258aeeedb40dfa86bcf3a1ac680294ae98
joe <joeyjhuang@iii.org.tw>
2017-11-10 11:44:13 +0800

    joe_update

---
+++ app/src/main/java/com/iii/more/main/FaceEmotionEventListener.java
@@ -0,0 +1,13 @@
+package com.iii.more.main;
+
+import java.util.HashMap;
+
+/**
+ * Created by joe on 2017/11/9.
+ */
+
+public interface FaceEmotionEventListener
+{
+    void onFaceEmotionResult(HashMap<String, String> faceEmotionHashMap);
+    
+}
