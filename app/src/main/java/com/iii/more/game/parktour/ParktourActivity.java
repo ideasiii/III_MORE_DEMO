9b15cbd4e977d3543adb257ff801a0c3768be47c
ideasiii <louisju@iii.org.tw>
2018-02-02 16:57:43 +0800

    update_jugo

--- app/src/main/java/com/iii/more/game/parktour/ParktourActivity.java
+++ app/src/main/java/com/iii/more/game/parktour/ParktourActivity.java
@@ -8,6 +8,9 @@ import com.iii.more.game.module.TrackerHandler;
 import com.iii.more.game.module.Utility;
 import com.iii.more.main.MainApplication;
 import com.iii.more.main.R;
+import com.iii.more.main.listeners.TTSEventListener;
+
+import sdk.ideas.common.Logs;
 
 public class ParktourActivity extends Activity
 {
@@ -24,5 +27,49 @@ public class ParktourActivity extends Activity
         faceView = new FaceView(this);
         setContentView(faceView);
         faceView.setBackgroundResource(R.drawable.iii_zoo_101);
+        application = (MainApplication) getApplication();
+        registerService();
+        scenarize(Scenarize.SCEN_START_ZOO, null);
+    }
+    
+    private void registerService()
+    {
+        application.setTTSEventListener(new TTSEventListener()
+        {
+            @Override
+            public void onInitSuccess()
+            {
+            
+            }
+            
+            @Override
+            public void onInitFailed(int status, String message)
+            {
+            
+            }
+            
+            @Override
+            public void onUtteranceStart(String utteranceId)
+            {
+            
+            }
+            
+            @Override
+            public void onUtteranceDone(String utteranceId)
+            {
+                Logs.showTrace("[ParktourActivity] onUtteranceDone utteranceId: " + utteranceId);
+            }
+        });
+    }
+    
+    private void scenarize(int nIndex, Object object)
+    {
+        switch (nIndex)
+        {
+            case Scenarize.SCEN_START_ZOO:
+                application.playTTS("今天是欣欣動物園年度園遊會，沿路都可以看到大家開心的來參加，所有的動物也都來了，讓我們來看看今天會遇到誰呢?走吧!",
+                    String.valueOf(nIndex));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/iii/more/game/parktour/Scenarize.java
@@ -0,0 +1,10 @@
+package com.iii.more.game.parktour;
+
+/**
+ * Created by Jugo on 2018/2/2
+ */
+
+public abstract class Scenarize
+{
+    final static int SCEN_START_ZOO = 1;
+}
