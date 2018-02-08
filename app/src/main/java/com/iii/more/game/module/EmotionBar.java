4c001496affcaf066ba34405680432c63dcc0233
ideasiii <louisju@iii.org.tw>
2018-02-08 10:51:55 +0800

    update_jugo

---
+++ app/src/main/java/com/iii/more/game/module/EmotionBar.java
@@ -0,0 +1,33 @@
+package com.iii.more.game.module;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Jugo on 2018/2/8
+ */
+
+public class EmotionBar extends LinearLayout
+{
+    public EmotionBar(Context context)
+    {
+        super(context);
+    }
+    
+    public EmotionBar(Context context, @Nullable AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+    
+    public EmotionBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+    }
+    
+    public EmotionBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes)
+    {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
